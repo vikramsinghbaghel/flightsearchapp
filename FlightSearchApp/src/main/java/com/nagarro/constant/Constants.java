1f8f92d9b0af0f87022777d89499c5cea7f830ad
vikrambaghel <vikrambaghel@vikrambaghel.Nagarro.local>
2021-04-16 18:41:07 +0530

    second commit

--- FlightSearchApp/src/main/java/com/nagarro/constant/Constants.java
+++ FlightSearchApp/src/main/java/com/nagarro/constant/Constants.java
@@ -2,5 +2,5 @@ package com.nagarro.constant;
 
 public class Constants {
 	
-	public static final String FILE_PATH="C:\\Users\\vikrambaghel\\eclipse-workspace\\FlightSearchApp\\src\\main\\resources";
+	public static final String FILE_PATH="C:\\Users\\vikrambaghel\\git\\flightsearchapp\\FlightSearchApp\\src\\main\\resources";
 }
